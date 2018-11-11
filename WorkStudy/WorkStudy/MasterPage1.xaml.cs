33142ebb3111a2e07644df069919c7b945d8c8d8
billy-tomlinson <billytomlinson@gmail.com>
2018-11-11 23:30:33 +0000

    removed unused files

--- WorkStudy/WorkStudy/MasterPage1.xaml.cs
+++ WorkStudy/WorkStudy/MasterPage1.xaml.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace WorkStudy
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MasterPage1 : ContentPage
     {
         public MasterPage1()
