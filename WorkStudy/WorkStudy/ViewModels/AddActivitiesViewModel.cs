1fecfe6b1d3b4cc9edc304052c5cae70181095f4
billy-tomlinson <billytomlinson@gmail.com>
2018-11-16 17:45:31 +0000

    Added comments functionlity

--- WorkStudy/WorkStudy/ViewModels/AddActivitiesViewModel.cs
+++ WorkStudy/WorkStudy/ViewModels/AddActivitiesViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 using WorkStudy.Model;
 using WorkStudy.Services;
 using Xamarin.Forms;
@@ -9,9 +11,16 @@ namespace WorkStudy.ViewModels
     {
         readonly IBaseRepository<Activity> activityRepo;
         public Command SaveActivity { get; set; }
+        public Command SaveComment { get; set; }
+        public Command CancelComment { get; set; }
+        public static string ActivityName;
+        List<Activity> AddedActivities = new List<Activity>();
+
         public AddActivitiesViewModel()
         {
             SaveActivity = new Command(SaveActivityDetails);
+            SaveComment = new Command(SaveCommentDetails);
+            CancelComment = new Command(CancelCommentDetails);
             activityRepo = new BaseRepository<Activity>();
             Name = string.Empty;
         }
@@ -41,6 +50,17 @@ namespace WorkStudy.ViewModels
             }
         }
 
+        static bool commentsVisible;
+        public bool CommentsVisible
+        {
+            get => commentsVisible;
+            set
+            {
+                commentsVisible = value;
+                OnPropertyChanged();
+            }
+        }
+
         void SaveActivityDetails()
         {
             if (activities == null)
@@ -49,20 +69,57 @@ namespace WorkStudy.ViewModels
                 activities = Utilities.Activities;
             }
 
-            activities.Add(Name);
+            if(!activities.Contains(Name.ToUpper().Trim()))
+                activities.Add(Name.ToUpper());
+            
             Utilities.Activities = activities;
+
+            AddedActivities.Add(new Activity { Name = Name.ToUpper() });
+
             Name = string.Empty;
+        }
+
+        void SaveCommentDetails()
+        {
+            var index  = AddedActivities.FindIndex(_ => _.Name == ActivityName);
+
+            AddedActivities.RemoveAt(index);
+            AddedActivities.Add(new Activity { Name = ActivityName, Comment = Comment });
+
+            CommentsVisible = false;
+            Comment = string.Empty;
+        }
+
+        void CancelCommentDetails()
+        {
+            CommentsVisible = false;
+            Comment = string.Empty;
         }
 
         public override void SubmitDetailsAndNavigate()
         {
-            foreach (var element in Activities)
+            foreach (var element in AddedActivities)
             {
-                var activity = new Activity { Name = element };
-                activityRepo.SaveItem(activity);
+                activityRepo.SaveItem(element);
             }
 
             Utilities.Navigate(new AddOperators());
+        }
+
+        public ICommand ItemClickedCommand
+        {
+            get { return ShowComments(); }
+        }
+
+        Command ShowComments()
+        {
+            return new Command((item) =>
+            {
+                ActivityName = (string)item;
+                var activity = AddedActivities.Find(_ => _.Name == ActivityName);
+                Comment = activity?.Comment;
+                CommentsVisible = true;
+            });
         }
     }
 }
