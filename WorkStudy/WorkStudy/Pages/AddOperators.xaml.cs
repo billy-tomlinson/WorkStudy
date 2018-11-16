4ede1b68e03da0d868c442064233e640f5d3bb98
billy-tomlinson <billytomlinson@gmail.com>
2018-11-16 22:42:02 +0000

    Added Activity pop up when clicking on Operator

--- WorkStudy/WorkStudy/Pages/AddOperators.xaml.cs
+++ WorkStudy/WorkStudy/Pages/AddOperators.xaml.cs
@@ -1,18 +1,71 @@
 ﻿
+using System;
+using WorkStudy.Custom;
+using WorkStudy.ViewModels;
 using Xamarin.Forms;
 
 namespace WorkStudy
 {
     public partial class AddOperators : ContentPage
     {
+
+        AddOperatorsViewModel viewModel;
+
         public AddOperators()
         {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
+            viewModel = BindingContext as AddOperatorsViewModel;
+            CreateActivitiesGrid();
         }
+
         protected override bool OnBackButtonPressed()
         {
             return true;
+        }
+
+        void CreateActivitiesGrid()
+        {
+            var items = viewModel.Activities;
+            double count = (double)items.Count / 3;
+            var rounded = Math.Ceiling(count);
+
+            for (int i = 0; i < rounded; i++)
+            {
+                gridLayout.RowDefinitions.Add(new RowDefinition());
+            }
+
+            gridLayout.ColumnDefinitions.Add(new ColumnDefinition());
+            gridLayout.ColumnDefinitions.Add(new ColumnDefinition());
+            gridLayout.ColumnDefinitions.Add(new ColumnDefinition());
+
+            var productIndex = 0;
+
+            for (int rowIndex = 0; rowIndex < rounded; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < 3; columnIndex++)
+                {
+                    if (productIndex >= items.Count)
+                    {
+                        break;
+                    }
+
+                    var product = items[productIndex];
+                    productIndex += 1;
+
+                    var label = new CustomButton
+                    {
+                        Text = product.Name,
+                        ActivityId = product.Id,
+                        Command = viewModel.ActivitySelected,
+                        VerticalOptions = LayoutOptions.Center,
+                        HorizontalOptions = LayoutOptions.Center,
+                        CommandParameter = product.Id
+
+                    };
+                    gridLayout.Children.Add(label, columnIndex, rowIndex);
+                }
+            }
         }
     }
 }
--- WorkStudy/WorkStudy/ViewModels/AddOperatorsViewModel.cs
+++ WorkStudy/WorkStudy/ViewModels/AddOperatorsViewModel.cs
@@ -10,14 +10,23 @@ namespace WorkStudy.ViewModels
     public class AddOperatorsViewModel : BaseViewModel
     {
         readonly IBaseRepository<Operator> operatorRepo;
+        readonly IBaseRepository<Activity> activitiesRepo;
         public Command SaveOperator { get; set; }
+        public Command SaveActivities { get; set; }
+        public Command CancelActivities { get; set; }
+        public Command ActivitySelected { get; set; }
         public Operator Operator;
 
         public AddOperatorsViewModel()
         {
             SaveOperator = new Command(SaveOperatorDetails);
+            SaveActivities = new Command(SaveActivityDetails);
+            CancelActivities = new Command(CancelActivityDetails);
+            ActivitySelected = new Command(ActivitySelectedEvent);
             operatorRepo = new BaseRepository<Operator>();
+            activitiesRepo = new BaseRepository<Activity>();
             Operators = new ObservableCollection<Operator>(operatorRepo.GetItems());
+            Activities = new ObservableCollection<Activity>(activitiesRepo.GetItems());
             Operator = new Operator();
             Name = string.Empty;
         }
@@ -33,6 +42,17 @@ namespace WorkStudy.ViewModels
             }
         }
 
+        private ObservableCollection<Activity> activities;
+        public ObservableCollection<Activity> Activities
+        {
+            get => activities;
+            set
+            {
+                activities = value;
+                OnPropertyChanged();
+            }
+        }
+
         private string name;
         public string Name
         {
@@ -43,6 +63,18 @@ namespace WorkStudy.ViewModels
                 OnPropertyChanged();
             }
         }
+
+        static bool activitiesVisible;
+        public bool ActivitiesVisible
+        {
+            get => activitiesVisible;
+            set
+            {
+                activitiesVisible = value;
+                OnPropertyChanged();
+            }
+        }
+
         void SaveOperatorDetails()
         {
             List<Operator> duplicatesCheck = new List<Operator>(Operators);
@@ -50,6 +82,31 @@ namespace WorkStudy.ViewModels
                 operatorRepo.SaveItem(new Operator { Name = Name.ToUpper().Trim() });
             Operators = new ObservableCollection<Operator>(operatorRepo.GetItems());
             Name = string.Empty;
+        }
+
+        void ActivitySelectedEvent(object sender)
+        {
+            var value = (int)sender;
+            //ActivityId = value;
+
+            //Observation.ActivityId = ActivityId;
+
+            //RatingsVisible = true;
+            ActivitiesVisible = false;
+        }
+
+        void SaveActivityDetails()
+        {
+            //Activity.Comment = Comment.ToUpper();
+            //activityRepo.SaveItem(Activity);
+
+            //CommentsVisible = false;
+            //Comment = string.Empty;
+        }
+
+        void CancelActivityDetails()
+        {
+            ActivitiesVisible = false;
         }
 
         public override void SubmitDetailsAndNavigate()
@@ -67,6 +124,7 @@ namespace WorkStudy.ViewModels
             return new Command((item) =>
             {
                 Operator = item as Operator;
+                ActivitiesVisible = true;
             });
         }
     }
