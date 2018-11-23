8e638b1f90cae6e407258eac7640681c98bb1c54
Billy Tomlinson <billygtomlinson@hotmail.com>
2018-11-23 16:12:26 +0000

    add basic email ( yet to test ) and included Syncfusion Exel package

--- WorkStudy/WorkStudy/Pages/StudyMenu.xaml.cs
+++ WorkStudy/WorkStudy/Pages/StudyMenu.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
+﻿using Plugin.Messaging;
 using Xamarin.Forms;
 
 namespace WorkStudy.Pages
@@ -10,6 +8,31 @@ namespace WorkStudy.Pages
         public StudyMenu()
         {
             InitializeComponent();
+
+            ButtonEmail.Clicked += async (sender, e) =>
+            {
+                try
+                {
+                    //var email = new EmailMessageBuilder()
+                    //    .To("to.plugins@xamarin.com")
+                    //    .Cc("cc.plugins@xamarin.com")
+                    //    .Bcc(new[] { "bcc1.plugins@xamarin.com", "bcc2.plugins@xamarin.com" })
+                    //    .Subject("Xamarin Messaging Plugin")
+                    //    .Body("Well hello there from Xam.Messaging.Plugin")
+                    //    .WithAttachment()
+                    //    .Build();
+
+                    var emailTask = CrossMessaging.Current.EmailMessenger;
+                    if (emailTask.CanSendEmail)
+                        emailTask.SendEmail(email.Text, "Hello there!", "This was sent from the Xamrain Messaging Plugin from shared code!");
+                    else
+                        await DisplayAlert("Error", "This device can't send emails", "OK");
+                }
+                catch
+                {
+                    await DisplayAlert("Error", "Unable to perform action", "OK");
+                }
+            };
         }
     }
 }
