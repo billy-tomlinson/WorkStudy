df22cceee886b7cb0a8e92186327f1da454ffa4a
billy-tomlinson <billytomlinson@gmail.com>
2018-11-26 18:09:33 +0000

    added

---
+++ WorkStudy/WorkStudy/Pages/ExistingStudies.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace WorkStudy.Pages
+{
+    public partial class ExistingStudies : ContentView
+    {
+        public ExistingStudies()
+        {
+            InitializeComponent();
+        }
+    }
+}
