b4101bf778ae47a001ff40e47f11d37c17507928
billy-tomlinson <billytomlinson@gmail.com>
2018-11-27 13:15:04 +0000

    Moved Activities to its own view and used in AddOPerators PAge

---
+++ WorkStudy/WorkStudy/Pages/ActivitiesView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace WorkStudy.Pages
+{
+    public partial class ActivitiesView : ContentView
+    {
+        public ActivitiesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
