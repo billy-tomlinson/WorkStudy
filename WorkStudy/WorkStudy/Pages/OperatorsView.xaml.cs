70981f783344ef8d96f187d1cfedb510e966f199
billy-tomlinson <billytomlinson@gmail.com>
2018-11-27 12:55:39 +0000

    Moved Operators to re-useable view

---
+++ WorkStudy/WorkStudy/Pages/OperatorsView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace WorkStudy.Pages
+{
+    public partial class OperatorsView : ContentView
+    {
+        public OperatorsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
