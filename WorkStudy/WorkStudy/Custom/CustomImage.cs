ba643e4864a8a8cf1fa2c09a34370e1bc09c47c3
billy-tomlinson <billytomlinson@gmail.com>
2018-11-27 19:26:46 +0000

    Added some styling

---
+++ WorkStudy/WorkStudy/Custom/CustomImage.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace WorkStudy.Custom
+{
+    public class CustomImage : Image
+    {
+        public static readonly BindableProperty OperatorIdProperty =  
+           BindableProperty.Create(  
+               nameof(OperatorId),  
+               typeof(int),  
+               typeof(CustomLabel),  
+               0);  
+  
+        public int OperatorId  
+        {  
+            get { return (int)GetValue(OperatorIdProperty); }  
+            set { SetValue(OperatorIdProperty, value); }  
+        } 
+    }
+}
