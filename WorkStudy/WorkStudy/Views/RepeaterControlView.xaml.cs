68ab5d2c2bea2736d06e9f8ea05ec4be16d611cd
billy-tomlinson <billytomlinson@gmail.com>
2019-05-16 14:37:18 +0100

    removed unused code

--- WorkStudy/WorkStudy/Views/RepeaterControlView.xaml.cs
+++ WorkStudy/WorkStudy/Views/RepeaterControlView.xaml.cs
@@ -4,11 +4,6 @@ namespace WorkStudy.Views
 {
     public partial class RepeaterControlView : ContentView
     {
-        void Handle_Focused(object sender, Xamarin.Forms.FocusEventArgs e)
-        {
-            throw new System.NotImplementedException();
-        }
-
         public RepeaterControlView()
         {
             InitializeComponent();
